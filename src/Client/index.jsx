099c13edd4b30398280214f2c330fccb48830977
whisper <whisper.snow@163.com>
2017-08-18 09:40:10 +0800

    webpack3 eslint

---
+++ src/Client/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * Created by whisper on 2016/11/6.
+ */
+'use strict';
+
+import ReactDOM from 'react-dom';
+import App from './component/App/app.jsx';
+import './asset/styles/style.scss';
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('.app')
+);
